b940e8910cf3bd619a9f4507e06467d8465a94c8
Yuto-yuyuyu <s228450x@st.go.tuat.ac.jp>
2025-05-08 02:55:00 +0900

    なんとかなれー！

--- src/app/tags/[slug]/page.tsx
+++ src/app/tags/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { notFound } from "next/navigation";
+import path from "path";
 
 import { getPostsByTag, getAllTags } from "@/lib/api";
 import TagAll from "@/app/_components/tag-all";
@@ -6,7 +7,6 @@ import PostList from "@/app/_components/post-list";
 import Caption from "@/app/_components/caption";
 import Separator from "@/app/_components/separator";
 
-import { BASE_PATH } from "@/constants/base-path";
 
 type Params = {
     params: Promise<{
@@ -16,9 +16,8 @@ type Params = {
 
 export default async function Tags(props: Params) {
     const params = await props.params;
-    const tag = decodeURIComponent(params.slug).replace(`${BASE_PATH}/`, "");
-    console.log(tag);
-    const posts = getPostsByTag(tag);   
+    const tag = decodeURIComponent(params.slug);
+    const posts = getPostsByTag(path.basename(tag));   
 
     if (!posts) {
         notFound();
